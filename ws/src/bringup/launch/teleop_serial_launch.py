79d6d237884183ce8d5ea48255c7755555a8e1de
robo-wheel <robo-wheel@proton.me>
2024-10-26 17:35:46 +0300

    removed unnessary comment

--- ws/src/bringup/launch/teleop_serial_launch.py
+++ ws/src/bringup/launch/teleop_serial_launch.py
@@ -3,12 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(
-        #     package="teleop_twist_keyboard",
-        #     executable="teleop_twist_keyboard",
-        #     name="teleop_twist_keyboard",
-        #     output="screen"
-        # ),
         Node(
             package="serial_communication",
             executable="serial_node",
